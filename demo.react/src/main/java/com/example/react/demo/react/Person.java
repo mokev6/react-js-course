b3c797d85926438d4ffe4850207523e15555bbc3
mokev6 <57002859+mokev6@users.noreply.github.com>
2021-12-09 14:56:46 +0100

    React avec springboot

---
+++ demo.react/src/main/java/com/example/react/demo/react/Person.java
@@ -0,0 +1,62 @@
+package com.example.react.demo.react;
+
+public class Person {
+
+	private Long id;
+	
+	private String name;
+	
+	private String lastname;
+	
+	private String email;
+
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public String getLastname() {
+		return lastname;
+	}
+
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+}
